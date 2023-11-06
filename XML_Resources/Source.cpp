7b7391bd4e69edfd511de70d9340c0282bad0f30
Michael <lutchenkomn555@gmail.com>
2023-11-06 20:49:32 +0300

    Iterator + pugixml.hpp deleted

--- XML_Resources/Source.cpp
+++ XML_Resources/Source.cpp
@@ -1,50 +1,136 @@
 #include <iostream>
 #include <string>
-#include <pugixml.hpp>
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
 
 class XMLResource {
 private:
-    pugi::xml_document xml_document;
-
-    XMLResource() {}
+    std::string xml_data;
 
 public:
-    static XMLResource* create() {
-        return new XMLResource();
+    static std::unique_ptr<XMLResource> create() {
+        return std::make_unique<XMLResource>();
     }
 
     bool load(const std::string& filename) {
-        pugi::xml_parse_result result = xml_document.load_file(filename.c_str());
-        return result.status == pugi::status_ok;
+        std::ifstream file(filename);
+        if (file.is_open()) {
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+            xml_data = buffer.str();
+            file.close();
+            return true;
+        }
+        else {
+            throw std::runtime_error("Error while loading XML");
+        }
     }
 
     bool save(const std::string& filename) {
-        return xml_document.save_file(filename.c_str());
+        std::ofstream file(filename);
+        if (file.is_open()) {
+            file << xml_data;
+            file.close();
+            return true;
+        }
+        else {
+            throw std::runtime_error("Error while saving XML");
+        }
     }
+
+    class Iterator {
+    private:
+        XMLResource& xml_resource;
+        size_t current_pos;
+        std::string current_element;
+
+    public:
+        Iterator(XMLResource& resource) : xml_resource(resource), current_pos(0) {
+            findNextElement();
+        }
+
+        bool hasNext() const {
+            return current_pos < xml_resource.xml_data.size();
+        }
+
+        void next() {
+            findNextElement();
+        }
+
+        bool hasPrevious() const {
+            return current_pos > 0;
+        }
+
+        void previous() {
+            findPreviousElement();
+        }
+
+        std::string getCurrentElement() const {
+            return current_element;
+        }
+
+    private:
+        void findNextElement() {
+            size_t start = xml_resource.xml_data.find('<', current_pos);
+            size_t end = xml_resource.xml_data.find('>', start);
+            if (start != std::string::npos && end != std::string::npos) {
+                current_element = xml_resource.xml_data.substr(start, end - start + 1);
+                current_pos = end + 1;
+            }
+            else {
+                current_element.clear();
+            }
+        }
+
+        void findPreviousElement() {
+            size_t start = xml_resource.xml_data.rfind('<', current_pos - 1);
+            size_t end = xml_resource.xml_data.find('>', current_pos);
+            if (start != std::string::npos && end != std::string::npos) {
+                current_element = xml_resource.xml_data.substr(start, end - start + 1);
+                current_pos = start;
+            }
+            else {
+                current_element.clear();
+            }
+        }
+    };
 };
 
-int32_t main(const int32_t argc, const char** const argv[]) {
+int main(int argc, char* argv[]) {
+    try {
+        auto resource = XMLResource::create();
 
-    XMLResource* resource = XMLResource::create();
+        if (resource->load("tree.xml")) {
+            std::cout << "XML loaded successfully" << std::endl;
+        }
+        else {
+            std::cout << "Error while loading XML" << std::endl;
+            return EXIT_FAILURE;
+        }
 
-    if (resource->load("tree.xml")) {
-        std::cout << "XML loaded succesfully" << std::endl;
+        if (resource->save("new_tree.xml")) {
+            std::cout << "XML saved in new_tree.xml." << std::endl;
+        }
+        else {
+            std::cout << "Error while saving XML" << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        XMLResource::Iterator it(*resource);
+
+        while (it.hasNext()) {
+            std::cout << "Current Element: " << it.getCurrentElement() << std::endl;
+            it.next();
+        }
     }
-    else {
-        std::cout << "Error while loading XML" << std::endl;
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
-
-
-    if (resource->save("new_tree.xml")) {
-        std::cout << "XML saved in new_tree.xml." << std::endl;
-    }
-    else {
-        std::cout << "Error while loading XML" << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    delete resource;
 
     return EXIT_SUCCESS;
 }
